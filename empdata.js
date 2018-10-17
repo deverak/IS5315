f99e8a49fadeadd745dda51c0827935e9c3165b3
kriztel <deverak@seattleu.edu>
2018-10-16 20:40:05 -0700

    Homework2_v3

--- empdata.js
+++ empdata.js
@@ -1,124 +1,124 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "first_name": "Anthony",
+      "last_name": "Chan",
+      "e-mail": "tonyhawk65@gmail.com",
+      "phone_number": "8082736491",
+      "availability": "M/W/F",
+      "hourly_rate": "$11/HR"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "first_name": "Jil",
+      "last_name": "Gurrero",
+      "e-mail": "j.gur@gmail.com",
+      "phone_number": "2637485938",
+      "availability": "T/THR",
+      "hourly_rate": "$12/HR"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "first_name": "Jell",
+      "last_name": "Livera",
+      "e-mail": "liveraaj@hotmail.com",
+      "phone_number": "7128304827",
+      "availability": "M/W/T/F/Sat/Sun",
+      "hourly_rate": "$15/HR"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "first_name": "Bernadine",
+      "last_name": "Smith",
+      "e-mail": "berniesmithy@yahoo.com",
+      "phone_number": "1536472019",
+      "availability": "Sat",
+      "hourly_rate": "$15/HR"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "first_name": "Ryan",
+      "last_name": "Lam",
+      "e-mail": "ryxnlxm@su.edu",
+      "phone_number": "8463748291",
+      "availability": "T/TH",
+      "hourly_rate": "$20/HR"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "first_name": "Mary",
+      "last_name": "Inoyue",
+      "e-mail": "m.inoyue@gmail.com",
+      "phone_number": "6479263784",
+      "availability": "F/Sat/Sun",
+      "hourly_rate": "$15.50/HR"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "first_name": "James",
+      "last_name": "Dun",
+      "e-mail": "$jimmydun@aol.com",
+      "phone_number": "2738491726",
+      "availability": "T/F",
+      "hourly_rate": "$14/HR"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "first_name": "Alyssa",
+      "last_name": "Wu",
+      "e-mail": "a.wu12@gmail.com",
+      "phone_number": "3648163726",
+      "availability": "Sat/Sun",
+      "hourly_rate": "$18/HR"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "first_name": "Olivia",
+      "last_name": "Bond",
+      "e-mail": "$obo@uw.edu",
+      "phone_number": "7453647294",
+      "availability": "W/Sat/Sun",
+      "hourly_rate": "$16/HR"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+       "first_name": "Dan",
+      "last_name": "Par",
+      "e-mail": "dp@gmail.com",
+      "phone_number": "7354619362",
+      "availability": "Tokyo",
+      "hourly_rate": "$18/HR"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "first_name": "Jeff",
+      "last_name": "Guo",
+      "e-mail": "guoj15@yahoo.com",
+      "phone_number": "2387696543",
+      "availability": "M/TH/F",
+      "hourly_rate": "$22/HR"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "first_name": "Cassie",
+      "last_name": "Falk",
+      "e-mail": "cassfalk@gmail.com",
+      "phone_number": "8374926371",
+      "availability": "Sat/Sun",
+      "hourly_rate": "$20/HR"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "first_name": "Nickolas",
+      "last_name": "Young",
+      "e-mail": "youngnicko@spu.edu",
+      "phone_number": "6473958253",
+      "availability": "Sat/Sun",
+      "hourly_rate": "$17/HR"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "first_name": "Cami",
+      "last_name": "Flemming",
+      "e-mail": "cflemming19@gmail.com",
+      "phone_number": "8362710364",
+      "availability": "T/TH",
+      "hourly_rate": "$14.5/HR"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "first_name": "Jordan",
+      "last_name": "Mot",
+      "e-mail": "jmot@hotmail.com",
+      "phone_number": "5462839108",
+      "availability": "M/W/F/Sat/Sun",
+      "hourly_rate": "$20/HR"
     }
   ]
 }
